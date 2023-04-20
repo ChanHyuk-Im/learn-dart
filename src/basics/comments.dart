61c872091abb004ea165bb616cc419090c229953
ChanHyeok Im <imchrg1@gmail.com>
2023-04-20 21:24:35 +0900

    basics/comments

---
+++ src/basics/comments.dart
@@ -0,0 +1,26 @@
+// single-line comments.
+
+/*
+  multi-line comments.
+*/
+
+/// documentation comments.
+/// using '///' or '/*** */'
+/// 
+/// using brackets, can refer to classes, methods, fields, top-level variables, functions, and parameters.
+/// 
+
+/// [feed] them some [Food]
+class Llama {
+  String? name;
+
+  /// feeds llama [food].
+  void feed(Food food) {
+    // ...
+  }
+
+  /// exercises with an [activity] for [timeLimit] minutes.
+  void exercise(Activity activity, int timeLimit) {
+    // ...
+  }
+}
